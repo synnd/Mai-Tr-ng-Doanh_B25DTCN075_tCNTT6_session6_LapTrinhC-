d69800418db129a2fdf0610632fa5a6e0dd3f0ff
synnd <127763785+synnd@users.noreply.github.com>
2025-10-29 23:23:31 +0700

    Update bai7.c

--- bai7.c
+++ bai7.c
@@ -5,6 +5,13 @@ int main (){
 	scanf ("%d",&a);
 	printf ("Nhap vao so thu hai ");
 	scanf ("%d",&b);
+	while (a<=0||b<=0){
+		printf ("So khong hop le!\nVui long nhap so nguyen duong\n ");
+		printf ("Nhap so thu nhat "); 
+		scanf ("%d",&a); 
+		printf ("Nhap so thu hai ");
+		scanf ("%d",&b) ; 
+	}
 	ucln = 1;
 	for (i=1;i<=a && i<=b;i++){
 		if (a%i==0&&b%i==0){
@@ -14,3 +21,4 @@ int main (){
 	printf ("Uoc chung lon nhat cua %d va %d la: %d",a,b,ucln);
 	return 0; 
 } 
+

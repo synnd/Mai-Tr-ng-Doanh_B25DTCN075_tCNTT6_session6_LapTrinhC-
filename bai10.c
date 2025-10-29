31fe3dee1b1cdadb9e6b611439aaaa0f53da4f56
synnd <127763785+synnd@users.noreply.github.com>
2025-10-30 00:02:58 +0700

    Update bai10.c

--- bai10.c
+++ bai10.c
@@ -1,38 +1,43 @@
 #include <stdio.h>
 int main (){
-int n;
-printf ("Nhap vao mot so nguyen: ");
-scanf ("%d",&n);
-if (n<0){
-	n=-n ;
-} 
+    int n;
+    printf ("Nhap vao mot so nguyen: ");
+    scanf ("%d",&n);
 
- if (n==0){
- 	printf ("Cac chu so la: %d",n); 
- 	return 0; 
- } 
-           // dao nguoc so truoc de lay thu tu tu phai sang trai 
- int so_du ;
- int so_dao=0;
- int chu_so ;
- 	while (n!=0){
-	so_du  = n % 10;                    // chia lay du tung chu so  
-	so_dao = so_dao * 10 + so_du;      // lan1 so_dao= 0*10+so_du   
-	n = n/10;                         // bo chu so vua in va tiep tuc chia  
+    if (n < 0){
+        n = -n;
+    }
+
+    if (n == 0){
+        printf ("Thu tu cac chu so tu trai sang phai la: 0\n");
+        return 0;
+    }
+
+    int temp  = n;
+    int so_dao = 0;
+
+    while (n > 0){
+        int so_du = n % 10;
+        so_dao = so_dao * 10 + so_du;
+        n = n / 10;
+    }
+
+    printf ("Thu tu cac chu so tu trai sang phai la: ");
+
+    while (so_dao > 0){
+        int chu_so = so_dao % 10;
+        printf (" %d ", chu_so);
+        so_dao = so_dao / 10;
+    }
+
+    while (temp  % 10 == 0 && temp  != 0) {
+               
+        if (so_dao == 0) {
+            printf(" 0 ");
+        }
+        temp  = temp  / 10;
+    }
+
+    printf ("\n");
+    return 0;
 }
-   // in ra thu tu ban dau
-   printf ("Thu tu cac chu so tu trai sang phai la: "); 
-   while (so_dao != 0){
-   	chu_so = so_dao %10 ;          // lay so dao o tren chia lay du roi in ra  
-   	printf (" %d ",chu_so);
-
-	 so_dao = so_dao/10;          //bo chu so vua in va lap tiep
-
-   }   
-
-    return 0; 
-
-}
-
-
-
